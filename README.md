c400d21a8b369561ea42814d6ac66cfc44bf09b7
Mackenzie Jessup <Mackenzie.jessup@mykenzie.snhu.edu>
2023-08-06 14:42:39 -0400

    completed basic maze

--- README.md
+++ README.md
@@ -5,8 +5,37 @@ Follow the instructions provided on my.kenzie.academy for this challenge. The `c
 Customize this README.md however you want to turn it into documentation for your project (including deleting these opening paragraphs). The only required sections are the **Project Plan** and **Reflection** below. Be sure to use proper Markdown syntax in this file (here's a [cheatsheet PDF](https://guides.github.com/pdfs/markdown-cheatsheet-online.pdf) of the basic syntax).
 
 ## Project Plan
+    
+1. Create the maps for the player to chose
+    1. Create string maps
+    2. create buttons that select each map as the layout
+        1. set eventlisteners for each button that will set the variable "mapLayout" as what the button is
+2. Create function that displays map
+    1. Accepts mapString as param
+    2. map through mapString, (W = wall, S= start, F= finish) 
+        1. if W, dispay block for wall
+        2. if S, display word start
+        3. if F, display word finish
+3. Create function to display player
+    1. find place of start, place player there through relative positioning and set coords as where start exists in array
+    2. change isPlayerDisplayed to true
+4. Create function to move player
+    1. Create an eventListener for "keydown"
+    2. Create movePlayer function
+        1. accepts event as param
+        2. if hasWon = true, return mapLayout = mapString
+        3. Create code that reacts to arrow keys
+        4. for each arrow press, map through mapString 
+            1. for each arrow, find where player coords are and where they would be if player moved
+            2. create function that checks if it is wall
+                1. use coords to search array for that spot, if it = "W", player cannot move
+            3. run isWall before moving player
+            4. run win function after each turn
+4. Create win function
+    1. finds players coords
+    2. search through array for that spot in it
+    3. if that spot = "F", change hasWon to true, and display "You won!" on page
 
-_(Put your project plan here. It could be pseudocode, an outline-style development plan, etc. But whatever form you choose, it should be detailed enough that another developer could feasibly use it to implement your solution.)_
 
 ## Reflection
 
--- code.js
+++ code.js
@@ -14,16 +14,50 @@ const mazeMap1 = [
     "W WWWWWWW W WWW W W W",
     "W         W     W W W",
     "W WWW WWWWW WWWWW W W",
-    "W W   W   W W     W W",
-    "W WWWWW W W W WWW W F",
-    "S     W W W W W W WWW",
-    "WWWWW W W W W W W W W",
-    "W     W W W   W W W W",
-    "W WWWWWWW WWWWW W W W",
+    "W W       W W     W W",
+    "W W  WWW   W W      F",
+    "S     W W   W W W WWW",
+    "WWWW  W  W  W W W W W",
+    "W     W W W     W W W",
+    "W W WWWWWW WWWW W W W",
     "W       W       W   W",
     "WWWWWWWWWWWWWWWWWWWWW"
 ];
+const mazeMap2 = [
+    "WWWWWSWWWWWWWWWWWWWWW",
+    "W   W     W     W   W",
+    "W W W WWW WWWWW W WWW",
+    "W W W  W      W W   W",
+    "W WWWWWWW W WWW W W W",
+    "W         W     W W W",
+    "W WWW WWWWW WWWWW W W",
+    "W W       W W     W W",
+    "W W  WWW   W W      W",
+    "W     W W   W W W WWW",
+    "WWWW  W  W  W W W W W",
+    "W     W W W     W W W",
+    "W W WWWWWW WWW W W  W",
+    "W       W       W   W",
+    "WWWWWWWWWFWWWWWWWWWWW"
+];
 
+const mazeMap3 = [
+    "WWWWWWWWWWWWWWWWWWWFW",
+    "W   W        WWW  W W",
+    "W W W WWW W WW  W W W",
+    "W W W  W    W   W   W",
+    "W WWW   W   WWW W WWW",
+    "W         W     W W W",
+    "W WWW WWWWW WW WW W W",
+    "W W         W     W W",
+    "W W  W W   W W      W",
+    "W            WWWW WWW",
+    "W  W  WWWW  W W WWW W",
+    "W     W W       W   W",
+    "W W WWW WW WWW WW WWW",
+    "W       W           W",
+    "WWSWWWWWWWWWWWWWWWWWW"
+];
 const sokobanMapSample = [ // FOR TESTING PURPOSES ONLY. Don't use this map
     " WWWWWW ",            // as one of the choices in your game.
     " WO   WW",  
@@ -45,3 +79,194 @@ const sokobanMap1 = [
 ];
 
 // Your Code Here.
+let mapLayout = mazeMap1
+let isMapDisplayed = false
+let isPlayerDisplayed = false
+let maze = document.getElementById("maze")
+let player = document.createElement("div")
+let x = 0
+let y = 0
+
+function displayPlayer () {
+    if (mapLayout === mazeMap1) {
+        let div = document.getElementById("player1")
+        player.className = "player"
+        player.style.top = "500px"
+        player.style.left = "0px"
+        player.style.position = "relative"
+        isPlayerDisplayed = true
+        div.append(player)
+        x = 9
+        y = 0
+    }
+    if (mapLayout === mazeMap2) {
+        let div = document.getElementById("player1")
+        player.className = "player"
+        player.style.top = "50px"
+        player.style.left = "250px"
+        player.style.position = "relative"
+        isPlayerDisplayed = true
+        div.append(player)
+        x = 0
+        y = 5
+    }
+    if (mapLayout === mazeMap3) {
+        let div = document.getElementById("player1")
+        player.className = "player"
+        player.style.top = "750px"
+        player.style.left = "100px"
+        player.style.position = "relative"
+        isPlayerDisplayed = true
+        div.append(player)
+        x = 14
+        y = 2
+    }
+}
+
+let maze1Button = document.getElementById("maze1Button")
+maze1Button.addEventListener("click", () => {
+    mapLayout = mazeMap1
+    isPlayerDisplayed = false
+    displayMap(mapLayout)
+    })
+let maze2Button = document.getElementById("maze2Button")
+maze2Button.addEventListener("click", () => {
+    mapLayout = mazeMap2
+    isPlayerDisplayed = false
+    displayMap(mapLayout)
+})
+let maze3Button = document.getElementById("maze3Button")
+maze3Button.addEventListener("click", () => {
+    mapLayout = mazeMap3
+    isPlayerDisplayed = false
+    displayMap(mapLayout)
+})
+
+function displayMap (mapString) {
+    if (isMapDisplayed === true) {
+        maze.innerHTML="";
+        isMapDisplayed = false
+    }
+    if (isPlayerDisplayed === false) {
+        displayPlayer()
+    }
+
+    isMapDisplayed = true
+    mapString.forEach(function(row, index) {
+        const arr = row.split("");
+        arr.forEach(function(path, i) {
+          let span = document.createElement("div");
+          if (path === "W") {
+            span.className = "mazeWall"
+          }
+          if (path === " ") {
+            span.className = "mazePath"
+          }
+          if (path === "S") {
+            span.className = "start"
+          }
+          if (path === "F") {
+            span.className = "finish";
+          }
+          maze.appendChild(span)
+        });
+        maze.appendChild(document.createElement("br"))
+      });
+}
+
+let hasWon = false
+
+function win () {
+    if (mapLayout[x][y] === "F") {
+        let div = document.getElementById("player1")
+        let win = document.createElement("h1")
+        win.innerText = "You won!"
+        div.append(win)
+        hasWon = true
+    }
+    
+}
+
+function isWall (x, y) {
+    if (mapLayout[x][y] === "W") {
+        return true
+    } else {
+        return false
+    }
+}
+
+document.addEventListener(
+    "keydown",
+    (event) => {
+
+        if (hasWon === true && mapLayout === mazeMap2) {
+            player.style.top = "750px"
+            return player.style.left = "450px"
+        }
+        if (hasWon === true && mapLayout === mazeMap1) {
+            player.style.top = "450px"
+            return player.style.left = "1000px"
+        }
+      switch (event.key) {
+        case "ArrowDown":
+            if (x === 14) {
+                return
+            }
+            let x1 = x + 1
+            if (isWall(x1, y) === true) {
+                return
+            }
+            player.style.top =
+            parseInt(player.style.top) + 50 + "px";
+            x += 1
+            win()
+          break;
+        case "ArrowUp":
+            if (x === 0){
+                return
+            }
+            x2 = x - 1
+            if (isWall(x2, y) === true) {
+                return
+            }
+          player.style.top =
+            parseInt(player.style.top) - 50 + "px";
+            x2 = x - 1
+            x -= 1
+            win()
+          break;
+        case "ArrowLeft":
+            if (y === 0) {
+                return 
+            }
+            let y1 = y - 1
+            if (isWall(x,y1) === true){
+                return
+            }
+          player.style.left =
+            parseInt(player.style.left) - 50 + "px";
+            y -= 1
+            win()
+          break;
+        case "ArrowRight":
+            if (y=== 20) {
+                return
+            }
+            let y2 = y + 1
+            if (isWall(x, y2) === true) {
+                return
+            }
+          player.style.left =
+            parseInt(player.style.left) + 50 + "px";
+            y += 1
+            win()
+          break;
+        default:
+          return; 
+      }
+    }
+  );
+
+
+
+
