3d2deedd9df9c9b7e1ad151f7d5c6eeee11bfeb5
Mackenzie Jessup <Mackenzie.jessup@mykenzie.snhu.edu>
2023-09-16 16:30:10 -0400

    completed avanced challenge

--- README.md
+++ README.md
@@ -35,10 +35,35 @@ Customize this README.md however you want to turn it into documentation for your
     1. finds players coords
     2. search through array for that spot in it
     3. if that spot = "F", change hasWon to true, and display "You won!" on page
-
+5. Create Sokoban puzzle out of maze
+    1. create crate and storage locations
+        1. repersented on the map a C, s, X, and @
+    2. create display crate 
+        1. in display map, it looks for C to repersent crates, classname is crate, id is crate${number}, and give it the styles the player has so it can move
+        2. push the x and y of the spot to cratesX and cratesY
+        3. if path = X, crate is already on storage x and y are pushed to crates x and y, and won x and y arrays
+    3. create how player moves crate
+        1. function for moving left, right, up and down
+        2. if player is aginst crate, function for moving it runs based on the arrow
+        3. moving itself runs similar to player, but crate x and y is held in arrays so you loop through array to get x for crate being moved, and use that index to get y
+        4. check for wall and other crates the same as the isWall function works, but for crates use the array for crates x and y instead of map
+    4. create storage spots
+        1. in display map function, if path = s, classname is storage
+        2. push its x and y to win x and y arrays
+        3. if path = @, classname is "storage start", everything else is same for this storage spot
+    5. create win function
+        1. in win function, edit so origional only runs on the maze maps, and new will only run on the new maps
+        2. run function to check if all storage spots are occupied, if true run the code that origional runs when game is won
+    6. create check if all occupied function
+        1. loop through winX array, and use index to get matching y array, storage occupied = false
+        2. loop through crates x array, and use index to get matching y
+        3. compare win x and x, and win y and y, if all are the same, storage occupied = true, else it returns  false
+        4. if it gets through loops, return true
 
 ## Reflection
 
 What different approaches or techniques did you consider when planning your implementation? What were the advantages and disadvantages of those alternatives?
 
-I started with thinking that I should move the player through the maze array itself, and just display that, but I was not able to work out how to actually do that properly. 
+I started with thinking that I should move the player through the maze array itself, and just display that, but I was not able to work out how to actually do that properly.
+
+I originally was going to check if game was won by using an array that would hold false for each storage spot, but i could not figure out a way to change the true back to false if the player moved the crate back off the storage
--- code.js
+++ code.js
@@ -1,10 +1,3 @@
-const mazeMapSample = [ // FOR TESTING PURPOSES ONLY. Don't use this map
-    "WWWWWWW",          // as one of the choices in your game.
-    "S W   W",
-    "W W W W",
-    "W   W F",
-    "WWWWWWW"
-];
 
 const mazeMap1 = [
     "WWWWWWWWWWWWWWWWWWWWW",
@@ -22,7 +15,7 @@ const mazeMap1 = [
     "W W WWWWWW WWWW W W W",
     "W       W       W   W",
     "WWWWWWWWWWWWWWWWWWWWW"
-];
+]
 const mazeMap2 = [
     "WWWWWSWWWWWWWWWWWWWWW",
     "W   W     W     W   W",
@@ -39,7 +32,7 @@ const mazeMap2 = [
     "W W WWWWWW WWW W W  W",
     "W       W       W   W",
     "WWWWWWWWWFWWWWWWWWWWW"
-];
+]
 
 const mazeMap3 = [
     "WWWWWWWWWWWWWWWWWWWFW",
@@ -57,35 +50,47 @@ const mazeMap3 = [
     "W W WWW WW WWW WW WWW",
     "W       W           W",
     "WWSWWWWWWWWWWWWWWWWWW"
-];
-const sokobanMapSample = [ // FOR TESTING PURPOSES ONLY. Don't use this map
-    " WWWWWW ",            // as one of the choices in your game.
-    " WO   WW",  
-    "WWW C  W",  
-    "WSX    W",  
-    "WWWWWWWW"  
-];
+]
 
 const sokobanMap1 = [  
-    "  WWWWW ",  
-    "WWW   W ",  
-    "WOSC  W ",  
-    "WWW COW ",  
-    "WOWWC W ",  
-    "W W O WW",  
-    "WC XCCOW",  
-    "W   O  W",  
-    "WWWWWWWW"  
-];
+    "  WWWWW ",
+    "WWW   W ",
+    "WsSC  W ",
+    "WWW CsW ",
+    "WsWWC W ",
+    "W W s WW",
+    "WC XCCsW",
+    "W   s  W",
+    "WWWWWWWW", 
+]
 
-// Your Code Here.
+const sokobanMap2 = [  
+    "WWWWWWWW",
+    "WsW   SW",
+    "W WC WWW",
+    "W     sW",
+    "W WWC  W",
+    "W     CW",
+    "W WCW  W",
+    "WsX   sW",
+    "WWWWWWWW", 
+]
+
 let mapLayout = mazeMap1
 let isMapDisplayed = false
 let isPlayerDisplayed = false
+let isCratesDisplayed = false
 let maze = document.getElementById("maze")
+let sokoban = document.getElementById("sokoban")
 let player = document.createElement("div")
+let dot = document.createElement("div")
+document.body.append(dot)
 let x = 0
 let y = 0
+let cratesX = []
+let cratesY = []
+let winXSpots = []
+let winYSpots = []
 
 function displayPlayer () {
     if (mapLayout === mazeMap1) {
@@ -121,31 +126,198 @@ function displayPlayer () {
         x = 14
         y = 2
     }
+    if (mapLayout === sokobanMap1) {
+        let div = document.getElementById("player1")
+        player.className = "player"
+        player.style.top = "150px"
+        player.style.left = "100px"
+        player.style.position = "relative"
+        isPlayerDisplayed = true
+        div.append(player)
+        x = 2
+        y = 2
+    }
+    if (mapLayout === sokobanMap2) {
+        let div = document.getElementById("player1")
+        player.className = "player"
+        player.style.top = "100px"
+        player.style.left = "300px"
+        player.style.position = "relative"
+        isPlayerDisplayed = true
+        div.append(player)
+        x = 1
+        y = 6 
+    }
 }
 
 let maze1Button = document.getElementById("maze1Button")
 maze1Button.addEventListener("click", () => {
     mapLayout = mazeMap1
     isPlayerDisplayed = false
+    
+    cratesX = []
+    cratesY = []
+    winXSpots = []
+    winYSpots = []
+    isCratesDisplayed = false
+    dot.innerHTML = ''
+
     displayMap(mapLayout)
     })
 let maze2Button = document.getElementById("maze2Button")
 maze2Button.addEventListener("click", () => {
     mapLayout = mazeMap2
     isPlayerDisplayed = false
+
+    cratesX = []
+    cratesY = []
+    winXSpots = []
+    winYSpots = []
+    isCratesDisplayed = false
+    dot.innerHTML = ''
+
     displayMap(mapLayout)
 })
 let maze3Button = document.getElementById("maze3Button")
 maze3Button.addEventListener("click", () => {
     mapLayout = mazeMap3
     isPlayerDisplayed = false
+
+    cratesX = []
+    cratesY = []
+    winXSpots = []
+    winYSpots = []
+    isCratesDisplayed = false
+    dot.innerHTML = ''
+
     displayMap(mapLayout)
 })
+
+let SokobanButton1 = document.getElementById('SokobanMap1Button')
+SokobanButton1.addEventListener("click", () => {
+    mapLayout = sokobanMap1
+    isPlayerDisplayed = false
+
+    cratesX = []
+    cratesY = []
+    winXSpots = []
+    winYSpots = []
+    isCratesDisplayed = false
+    dot.innerHTML = ''
+    displaySMap(mapLayout)
+})
+
+let SokobanButton2 = document.getElementById('SokobanMap2Button')
+SokobanButton2.addEventListener("click", () => {
+    mapLayout = sokobanMap2
+    isPlayerDisplayed = false
+
+    cratesX = []
+    cratesY = []
+    winXSpots = []
+    winYSpots = []
+    isCratesDisplayed = false
+    dot.innerHTML = ''
+    displaySMap(mapLayout)
+})
+
+function handleCrateAndStorage () {
+    if (mapLayout === sokobanMap1) {
+        let div = document.createElement('div')
+        div.className = "xDot"
+        div.style.position = "relative"
+        div.style.top = "-150px"
+        div.style.left = "150px"
+        dot.append(div)
+    }
+    if (mapLayout === sokobanMap2) {
+        let div = document.createElement('div')
+        div.className = "xDot"
+        div.style.position = "relative"
+        div.style.top = "-100px"
+        div.style.left = "100px"
+        dot.append(div)
+    }
+}
+
+function displaySMap (mapString) {
+    if (isMapDisplayed === true) {
+        let div = document.getElementById("player1")
+        sokoban.innerHTML=""
+        maze.innerHTML = ""
+        div.innerHTML=""
+        hasWon = false
+        isMapDisplayed = false
+    }
+    if (isPlayerDisplayed === false) {
+        displayPlayer()
+    }
+    let mapx = 0
+    let mapy = 0
+    let crateindex = 1
+
+    isMapDisplayed = true
+    mapString.forEach(function(row, index) {
+        const arr = row.split("");
+        arr.forEach(function(path, i) {
+          let span = document.createElement("div")
+          if (path === "W") {
+            span.className = "mazeWall"
+          }
+          if (path === " ") {
+            span.className = "mazePath"
+          }
+          if (path === "S") {
+            span.className = "start"
+          }
+          if (path === 's') {
+            span.className = "storage"
+            winXSpots.push(mapx)
+            winYSpots.push(mapy)
+          }
+          if (path === "C") {
+            span.className = "crate"
+            span.id = `crate${crateindex}`
+            span.style.top = "0px"
+            span.style.left = "0px"
+            span.style.position = "relative"
+            cratesX.push(mapx)
+            cratesY.push(mapy)
+            crateindex += 1
+          }
+          if (path === '@') {
+            span.className = "sStart"
+            winXSpots.push(mapx)
+            winYSpots.push(mapy)
+
+          }
+          if (path === "X") {
+            span.className = "storageCrate"
+            span.id = `crate${crateindex}`
+            crateindex += 1
+            cratesX.push(mapx)
+            cratesY.push(mapy)
+            winXSpots.push(mapx)
+            winYSpots.push(mapy)
+            span.style.top = "0px"
+            span.style.left = "0px"
+            span.style.position = "relative"
+          }
+          sokoban.appendChild(span)
+          mapy += 1
+        });
+        sokoban.appendChild(document.createElement("br"))
+        mapx += 1
+        mapy = 0
+      });
+    handleCrateAndStorage()
+}
 
 function displayMap (mapString) {
     if (isMapDisplayed === true) {
         let div = document.getElementById("player1")
-        maze.innerHTML="";
+        maze.innerHTML=""
+        sokoban.innerHTML=""
         div.innerHTML=""
         hasWon = false
         isMapDisplayed = false
@@ -180,14 +352,44 @@ function displayMap (mapString) {
 let hasWon = false
 
 function win () {
-    if (mapLayout[x][y] === "F") {
-        let div = document.getElementById("player1")
-        let win = document.createElement("h1")
-        win.innerText = "You won!"
-        div.append(win)
-        hasWon = true
+    if (mapLayout === mazeMap1 || mapLayout === mazeMap2 || mapLayout === mazeMap3){
+        if (mapLayout[x][y] === "F") {
+            let div = document.getElementById("player1")
+            let win = document.createElement("h1")
+            win.innerText = "You won!"
+            div.append(win)
+            hasWon = true
+        }
     }
-    
+    if (mapLayout === sokobanMap1 || mapLayout === sokobanMap2) {
+        if (checkAllStorageOccupied()) {
+            let div = document.getElementById("player1")
+            let win = document.createElement("h1")
+            win.innerText = "You won!"
+            div.append(win)
+            hasWon = true
+        }
+    }
+}
+
+function checkAllStorageOccupied() {
+    for (let index = 0; index < winXSpots.length; index++) {
+        const winX = winXSpots[index]
+        const winY = winYSpots[index]
+        let storageOccupied = false
+        for (let index = 0; index < cratesX.length; index++) {
+            const x = cratesX[index];
+            const y = cratesY[index]
+            if (winX === x && winY === y) {
+                storageOccupied = true
+                break
+            }
+        }
+        if (!storageOccupied) {
+            return false
+        }
+    }
+    return true
 }
 
 function isWall (x, y) {
@@ -198,10 +400,102 @@ function isWall (x, y) {
     }
 }
 
+function moveCrateUp(x,y) {
+    for (let index = 0; index < cratesX.length; index++) {
+        const crate = document.getElementById(`crate${index + 1}`)
+        const element = cratesX[index]
+        const e = cratesY[index]
+        if (x === element && y === e) {
+            if (mapLayout[x - 1][y] === "W") {
+                return true
+            }
+            for (let index = 0; index < cratesX.length; index++) {
+                const x = cratesX[index]
+                const y = cratesY[index]
+                if (element - 1 === x && e === y) {
+                    return true
+                }
+            }
+            cratesX[index] -= 1
+            crate.style.top =
+            parseInt(crate.style.top) - 50 + "px"
+        }
+    }
+    return false
+}
+function moveCrateDown(x,y) {
+    for (let index = 0; index < cratesX.length; index++) {
+        const crate = document.getElementById(`crate${index + 1}`)
+        const element = cratesX[index]
+        const e = cratesY[index]
+        if (x === element && y === e) {
+            if (mapLayout[x + 1][y] === "W") {
+                return true
+            }
+            for (let index = 0; index < cratesX.length; index++) {
+                const x = cratesX[index]
+                const y = cratesY[index]
+                if (element + 1 === x && e === y) {
+                    return true
+                }
+            }
+            cratesX[index] += 1
+            crate.style.top =
+            parseInt(crate.style.top) + 50 + "px"
+        }
+    }
+    return false
+}
+function moveCrateLeft(x,y) {
+    for (let index = 0; index < cratesX.length; index++) {
+        const crate = document.getElementById(`crate${index + 1}`)
+        const element = cratesX[index]
+        const e = cratesY[index]
+        if (x === element && y === e) {
+            if (mapLayout[x][y - 1] === "W") {
+                return true
+            }
+            for (let index = 0; index < cratesX.length; index++) {
+                const x = cratesX[index];
+                const y = cratesY[index]
+                if (element === x && e - 1 === y) {
+                    return true
+                }
+            }
+            cratesY[index] -= 1
+            crate.style.left =
+            parseInt(crate.style.left) - 50 + "px"
+        }
+    }
+    return false
+}
+function moveCrateRight(x,y) {
+    for (let index = 0; index < cratesX.length; index++) {
+        const crate = document.getElementById(`crate${index + 1}`)
+        const element = cratesX[index]
+        const e = cratesY[index]
+        if (x === element && y === e) {
+            if (mapLayout[x][y + 1] === "W") {
+                return true
+            }
+            for (let index = 0; index < cratesX.length; index++) {
+                const x = cratesX[index];
+                const y = cratesY[index]
+                if (element === x && e + 1 === y) {
+                    return true
+                }
+            }
+            cratesY[index] += 1
+            crate.style.left =
+            parseInt(crate.style.left) + 50 + "px"
+        }
+    }
+    return false
+}
+
 document.addEventListener(
     "keydown",
     (event) => {
-
         if (hasWon === true) {
             return 
         }
@@ -214,8 +508,11 @@ document.addEventListener(
             if (isWall(x1, y) === true) {
                 return
             }
+            if (moveCrateDown(x1,y) === true) {
+                return
+            }
             player.style.top =
-            parseInt(player.style.top) + 50 + "px";
+            parseInt(player.style.top) + 50 + "px"
             x += 1
             win()
           break;
@@ -227,8 +524,11 @@ document.addEventListener(
             if (isWall(x2, y) === true) {
                 return
             }
+            if (moveCrateUp(x2, y) === true) {
+                return
+            }
           player.style.top =
-            parseInt(player.style.top) - 50 + "px";
+            parseInt(player.style.top) - 50 + "px"
             x2 = x - 1
             x -= 1
             win()
@@ -241,8 +541,11 @@ document.addEventListener(
             if (isWall(x,y1) === true){
                 return
             }
+            if (moveCrateLeft(x,y1) === true){
+                return
+            }
           player.style.left =
-            parseInt(player.style.left) - 50 + "px";
+            parseInt(player.style.left) - 50 + "px"
             y -= 1
             win()
           break;
@@ -254,8 +557,11 @@ document.addEventListener(
             if (isWall(x, y2) === true) {
                 return
             }
+            if (moveCrateRight(x, y2) === true) {
+                return
+            }
           player.style.left =
-            parseInt(player.style.left) + 50 + "px";
+            parseInt(player.style.left) + 50 + "px"
             y += 1
             win()
           break;
@@ -264,7 +570,3 @@ document.addEventListener(
       }
     }
   );
-
-
-
-
